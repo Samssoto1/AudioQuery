ed61ef486616bdf071fd473f0fd15c97ec2141bd
Samssoto1 <Samssoto1@gmail.com>
2022-07-07 09:24:02 -0600

    GameServiceAdded

--- client/src/app/components/game/prompt-for-nickname/prompt-for-nickname.component.ts
+++ client/src/app/components/game/prompt-for-nickname/prompt-for-nickname.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, EventEmitter, Output } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-prompt-for-nickname',
@@ -8,20 +10,36 @@ import { NgForm } from '@angular/forms';
 })
 export class PromptForNicknameComponent implements OnInit {
 
-
-  useNickname: boolean = false;
+  isLoggedIn: boolean = false;
+  username: string;
+  nick: string = "";
   @ViewChild('f') promptForNicknameForm: NgForm;
   @Output() sendNickname = new EventEmitter<string>();
 
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.getIsAuth();
+    if(localStorage.getItem('username')){
+      this.username = localStorage.getItem('username')
+    }
+    }
+
+  useUsername(){
+    this.nick = this.username;
   }
+
+  useNick(){
+    this.nick = "";
+  }
+  
 
   onFormSubmit(){
     if(this.promptForNicknameForm.valid == true){
+      console.log(this.promptForNicknameForm.value.nickname)
       this.sendNickname.emit(this.promptForNicknameForm.value.nickname);
+      // this.router.navigate(['game'])
     }
   }
 
